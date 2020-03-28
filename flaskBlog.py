273939b8aaedf55154e02756001f49fa575cf83a
Danny <dantes817@gmail.com>
2020-03-28 21:20:58 +0100

    was created simple web-server with couple routes

--- flaskBlog.py
+++ flaskBlog.py
@@ -3,5 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return '<h1>Nice try!</h1>'
+@app.route("/home")
+def home():
+    return '<h1 style="text-align: center;">Nice try!!!<a href="/about" style="color: teal; margin: 0 auto; padding: 30% 30%;"> About</a></h1>'
+
+@app.route("/about")
+def about():
+    return '<h2 style="text-align: center;">About Me<a href="/" style="color: red; margin: 0 auto; padding: 30% 30%;"> Home page</a></h2>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
