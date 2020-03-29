59ebbfcf067a61640150d6c5db353fa95344aa2f
Danny <dantes817@gmail.com>
2020-03-29 13:44:16 +0200

    was added 2 folders for static files

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,15 +1,29 @@
 
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Danny Creator',
+        'title': 'Blog, 1_st. Post',
+        'content': '1_st. Post Content',
+        'date': 'May 9, 2020'
+    },
+    {
+        'author': 'Djanki Bastard',
+        'title': 'Blog, 2_st. Post',
+        'content': '2_st. Post Content',
+        'date': 'DoomDay 13, 2020'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return '<h1 style="text-align: center;">Nice try!!!<a href="/about" style="color: teal; margin: 0 auto; padding: 30% 30%;"> About</a></h1>'
-
+    return render_template('home.html', posts=posts)
 @app.route("/about")
 def about():
-    return '<h2 style="text-align: center;">About Me<a href="/" style="color: red; margin: 0 auto; padding: 30% 30%;"> Home page</a></h2>'
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
